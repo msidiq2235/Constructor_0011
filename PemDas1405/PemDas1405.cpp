b32cd87054a2139885152f3c66033082ce10d16e
msidiq2235 <muhsidiq2005@gmail.com>
2024-05-14 17:50:27 +0700

    2

--- PemDas1405/PemDas1405.cpp
+++ PemDas1405/PemDas1405.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class mahasiswa
 {
+public:
 	static int nim;
 	int id;
 	string nama;
@@ -16,3 +18,31 @@ class mahasiswa
 		setID();
 	}
 };
+
+int mahasiswa::nim = 113;
+
+void mahasiswa::setID()
+{
+	id = nim++;
+}
+
+void mahasiswa::printAll()
+{
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+int main()
+{
+	mahasiswa mhs1("Lia Kurnia");
+	mahasiswa mhs2("Asroni");
+	mahasiswa mhs3("Andi Kurniawan");
+	mahasiswa mhs4("Joko Purbo");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+	return 0;
+}

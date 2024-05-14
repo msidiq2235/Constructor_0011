dd7ab7a39dd82431011f4d371694683502250659
msidiq2235 <muhsidiq2005@gmail.com>
2024-05-14 18:48:04 +0700

    7

---
+++ Construktor/Construktor.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa
+{
+public:
+	int nim;
+	string nama;
+
+public:
+	Mahasiswa()
+	{
+		nim = 0;
+		nama = "";
+	};
+	Mahasiswa(int iNim)
+	{
+		nim = iNim;
+	};
+	Mahasiswa(string iNama)
+	{
+		nama = iNama;
+	};
+	Mahasiswa(int iNim, string iNama)
+	{
+		nim = iNim;
+		nama = iNama;
+
+	};
+	void cetak() {
+		cout << "Nim = " << nim << endl;
+		cout << "Nama = " << nama << endl;
+		cout << endl;
+	}
+};

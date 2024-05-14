ee41d852502a15e2e92a94bce0d0ae64648f285f
msidiq2235 <muhsidiq2005@gmail.com>
2024-05-14 19:09:38 +0700

    10

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -9,7 +9,7 @@ private:
 public:
 	angka(int); //constructor
 	~angka(); //destructor
-	void cetakdata();
+	void cetakData();
 	void isiData();
 };
 //definisi member function
@@ -17,4 +17,33 @@ angka::angka(int i) { //contructor
 	panjang = i;
 	arr = new int[1];
 	isiData();
+}
+
+angka::~angka() {//destructor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
+void angka::cetakData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << 1 << " = " << arr[i] << endl;
+	}
+}
+
+void angka::isiData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << "=";
+		cin >> arr[i];
+	}
+	cout << endl;
+}
+
+int main() {
+	angka belajarcpp(3);
+	angka* ptrBelajarcpp = new angka(5);
+	delete ptrBelajarcpp;
+
+	return 0;
 }

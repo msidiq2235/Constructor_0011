37933d4bef56ed69bbca849a1624dcf9f77ccf5d
msidiq2235 <muhsidiq2005@gmail.com>
2024-05-14 18:53:44 +0700

    8

--- Construktor/Construktor.cpp
+++ Construktor/Construktor.cpp
@@ -32,5 +32,20 @@ public:
 		cout << "Nim = " << nim << endl;
 		cout << "Nama = " << nama << endl;
 		cout << endl;
-	}
+	};
 };
+
+int main()
+{
+	Mahasiswa mhs1;
+	Mahasiswa mhs2(20);
+	Mahasiswa mhs3("Indra");
+	Mahasiswa mhs4(30, "Fauzan");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
+	return 0;
+}
